cae7b3821a695679f4a760bdca08c88e45a6ea03
Shumbusho43 <davidshumbusho10@gmail.com>
2023-05-31 01:12:55 +0200

    db string modification

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 //generate function for connecting to mongodb
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: '.env'});
+dotenv.config({path: './.env'});
 async function connectToMongoDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect('mongodb+srv://davidshumbusho10:tHwytIt26G7DVv4X@cluster0.utg0kh6.mongodb.net/');
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error);
